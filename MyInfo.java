8676696a44da9b0caff11013ddc746a3d7c0d60e
Stefany Olivera <34490162+Stefany-Olivera@users.noreply.github.com>
2018-04-23 19:07:52 -0400

    Add files via upload

---
+++ MyInfo.java
@@ -0,0 +1,8 @@
+public class MyInfo {
+    public static void main(String[] args){
+        System.out.println("My name is Stefany.");
+        System.out.println("I am 27 years old.");
+        System.out.println("My hometown is Perth Amboy, NJ.");
+
+    }
+}
